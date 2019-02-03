709775aaafb5d04a78fe300134597be6d4b92210
ck148 <2200743002@qq.com>
2019-02-03 16:13:37 +0800

    路径

---
+++ EMS60/src/layout/sidebar.vue
@@ -0,0 +1,62 @@
+<template>
+    <!-- 
+            el-menu：导航菜单
+                default-active：设置默认激活的菜单选项
+                open事件：当菜单选项被展开时会执行
+                close事件：当菜单选项被关闭时会执行
+            el-submenu：导航菜单的选项
+                template标签: 设置的是一级菜单
+        -->
+    <el-menu :router="true" :unique-opened="true" default-active="2" class="el-menu-vertical-demo mymenu">
+        <el-submenu v-for="item1 in rightsList" :key="item1.id" :index="item1.path">
+            <!-- 一级菜单 -->
+            <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>{{item1.authName}}</span>
+            </template>
+            <el-menu-item v-for="item2 in item1.children" :key="item2.id" :index="'/' + item2.path">
+                <i class="el-icon-menu"></i>
+                <span>{{item2.authName}}</span>
+            </el-menu-item>
+        </el-submenu>
+    </el-menu>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            rightsList: []
+        }
+    },
+    methods: {
+        // 用来得到当前登录用户的所有的权限
+        async getRightsList() {
+            var res = await this.$http.request({
+                url: '/menus',
+                method: 'get',
+                headers: {
+                    'Authorization': window.localStorage.getItem('token')
+                }
+            })
+            var { meta, data } = res.data
+            if (meta.status === 200) {
+                this.rightsList = data
+            } else {
+                this.$message.error(meta.msg)
+            }
+        }
+    },
+    mounted() {
+        this.getRightsList()
+    }
+}
+</script>
+
+<style>
+/* 侧边栏样式 */
+
+.mymenu {
+    height: 100%
+}
+</style>
---
+++ EMS60/src/router/index.js
@@ -0,0 +1,69 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import UserElem from '@/components/common/useElem.vue'
+import Login from '@/components/login/login.vue'
+import Home from '@/components/page/home.vue'
+import User from '@/components/page/users/users.vue'
+import Rights from '@/components/page/rights/rights.vue'
+import Roles from '@/components/page/roles/roles.vue'
+import Goods from '@/components/page/goods/goods.vue'
+import GoodsAdd from '@/components/page/goods/add.vue'
+import Categories from '@/components/page/categories/categories.vue'
+import Orders from '@/components/page/orders/orders.vue'
+
+import { Message } from 'element-ui'
+
+Vue.use(Router)
+
+var router = new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      name: 'UserElem',
+      path: '/ele',
+      component: UserElem
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
+      path: '/home',
+      component: Home,
+      children: [
+        { path: '/users', component: User },// /users
+        { path: '/rights', component: Rights },// /rights
+        { path: '/roles', component: Roles },// /roles
+        { path: '/goods', component: Goods},
+        { path: '/goods/add', component: GoodsAdd },
+        { path: '/categories', component: Categories },
+        { path: '/orders', component: Orders }
+      ]
+    }
+  ]
+})
+
+// 添加一个全局前置守卫(当路由发生改变时会执行)
+router.beforeEach((to, from, next) => {
+  // 当请求 login 时不应该验证
+  if (to.path !== '/login') {
+    // 验证 Localstorage 中是否存在 token
+    var token = window.localStorage.getItem('token')
+    if (!token) {
+      // 跳转到登录页面
+      router.push('/login')
+      Message.error('您还没有登录，请登录')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
